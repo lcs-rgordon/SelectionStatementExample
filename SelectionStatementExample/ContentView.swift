c60eac36dcee362741ed177d9ac70f6052019df0
Russell Gordon <rgordon@lcs.on.ca>
2021-12-02 09:23:33 -0500

    Got the basic layout for the game created.

--- SelectionStatementExample/ContentView.swift
+++ SelectionStatementExample/ContentView.swift
@@ -9,13 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Slider(value: .constant(61.0),
+                   in: 0.0...100.0,
+                   step: 1.0,
+                   label: {
+                        Text("Guess")
+            },
+                   minimumValueLabel: {
+                        Text("0")
+            },
+                   maximumValueLabel: {
+                        Text("100")
+            })
+            
+            Text("61")
+                .font(.largeTitle)
+                .bold()
+            
+            Button(action: {
+                // NOTE: Output will not be shown unless this app is run in the "full" simulator
+                print("Button was pressed")
+            }, label: {
+                Text("Submit Guess")
+            })
+            .buttonStyle(.bordered)
+            
+            Spacer()
+
+        }
+        .navigationTitle("Guessing Game")
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
