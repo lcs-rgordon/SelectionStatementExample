cdbe0d74cfd5f0bc4c195636f6e41eeaaebb91df
Russell Gordon <rgordon@lcs.on.ca>
2021-12-02 09:11:27 -0500

    Initial Commit

---
+++ SelectionStatementExample/SelectionStatementExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelectionStatementExampleApp.swift
+//  SelectionStatementExample
+//
+//  Created by Russell Gordon on 2021-12-02.
+//
+
+import SwiftUI
+
+@main
+struct SelectionStatementExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
